d224112ab7038c52578aa056f5f902524d9236ca
Austin Wei <awei01@gmail.com>
2015-02-27 15:41:27 -0500

    updating build to include tags

--- build/split.sh
+++ build/split.sh
@@ -1,4 +1,5 @@
 git subsplit init git@github.com:awei01/unit-testing.git
-git subsplit publish --heads="master" --no-tags FunctionSpy:git@github.com:unit-testing/function-spy.git
-git subsplit publish --heads="master" --no-tags MockeryHelper:git@github.com:unit-testing/mockery-helper.git
+git subsplit publish --heads="master" ClassSpy:git@github.com:unit-testing/class-spy.git
+git subsplit publish --heads="master" FunctionSpy:git@github.com:unit-testing/function-spy.git
+git subsplit publish --heads="master" MockeryHelper:git@github.com:unit-testing/mockery-helper.git
 rm -r -f .subsplit/
